0e72ea31d12ca9439eb24e5cb3866810e21471ec
genevievehg <genevievegarwood@gmail.com>
2026-09-03 23:14:50 +0100

    test: add test for convert files in folder function

--- tests/test_file_setup.py
+++ tests/test_file_setup.py
@@ -1,6 +1,7 @@
-from file_setup import convert_json_to_parquet
+from file_setup import convert_json_to_parquet, convert_files_in_folder
 import pandas as pd
 import pytest
+import os
 
 def test_convert_json_to_parquet_converts_files_sucessfully(tmp_path):
     input_path = tmp_path / 'test.json'
@@ -42,3 +43,25 @@ def test_convert_json_to_parquet_handles_missing_output_directory(tmp_path):
         convert_json_to_parquet(str(input_path), str(output_path))
 
     assert e.value.args[0] == f'Cannot write {output_path}. Directory does not exist'
+
+
+def test_convert_files_in_folder(tmp_path):
+    input_directory = tmp_path / 'test_input'
+    input_directory.mkdir()
+    output_directory = tmp_path / 'test_output'
+    output_directory.mkdir()
+
+    json_files = [{
+            "song": ["Happy", "Love Forever"],
+            "artist": ["Prince", "Cher"]},
+            {"song": ["Happy", "Love Forever"],
+            "artist": ["Prince", "Cher"]
+            }]
+
+    for i, file in enumerate(json_files):
+        df = pd.DataFrame(file)
+        df.to_json(input_directory / f'test_input_{i}.json', orient="records")
+
+    convert_files_in_folder(str(input_directory), str(output_directory))
+    
+    assert (output_directory / 'test_input_0.parquet').exists()
