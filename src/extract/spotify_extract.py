1c27b67bf9e7b4c8dc88f359c9310b7f369ff4a9
genevievehg <genevievegarwood@gmail.com>
2026-09-16 13:12:12 +0100

    feat: add get user data function

---
+++ src/extract/spotify_extract.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from src.extract.spotify_client import spotify_client
+
+
+sp = spotify_client()
+
+
+def get_user_data():
+
+    result = sp.current_user()
+    dictionary_keys = result.keys()
+
+    keys_to_remove = ['explicit_content', 'external_urls', 'href', 'images']
+
+    for key in keys_to_remove:
+        if key in dictionary_keys:
+            del result[key]
+
+    if 'followers' in result.keys():
+        result['followers'] = result['followers']['total']
+    
+    df = pd.DataFrame(result, index=[0])
+    
+    return df
