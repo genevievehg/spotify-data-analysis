cbd94c63dcf6f343568f970084359e02ee6ceb8d
genevievehg <genevievegarwood@gmail.com>
2026-09-16 13:14:34 +0100

    feat: add get artist data function

--- src/extract/spotify_extract.py
+++ src/extract/spotify_extract.py
@@ -20,5 +20,21 @@ def get_user_data():
         result['followers'] = result['followers']['total']
     
     df = pd.DataFrame(result, index=[0])
+
+    return df
+
+
+def get_artist_data(id: str):
+    
+    result = sp.artist(artist_id=id)
+    dictionary_keys = result.keys()
+
+    keys_to_remove = ['external_urls', 'href', 'images']
+
+    for key in keys_to_remove:
+        if key in dictionary_keys:
+            del result[key]
+    
+    df = pd.DataFrame(result, index=[0])
     
     return df
